039745fdf86724cb01de21bf0c60314f0ebfb7c2
rafly <raflyritonga21@gmail.com>
2024-05-05 23:14:07 +0700

    fixing bugs

--- elt/elt_script.py
+++ elt/elt_script.py
@@ -19,7 +19,7 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
     print("Max retries reached. Exiting.")
     return False
 
-# Use the function before running the ELT process
+# Check the database connetion
 if not wait_for_postgres(host="source_postgres"):
     exit(1)
 else:
@@ -32,17 +32,17 @@ else:
         'password': 'secret',
         'host': 'source_postgres'
     }
-
-    # Use pg_dump to dump the source database to a SQL file
+    
     dump_command = [
         'pg_dump',
+        '-d', source_config['dbname'],
         '-h', source_config['host'],
         '-U', source_config['user'],
-        '-d', source_config['dbname'],
+        '-w',
         '-f', 'data_dump.sql'
     ]
 
-    subprocess_env= source_config['password']
+    subprocess_env = dict(PGPASSWORD=source_config['password'])
     subprocess.run(dump_command, env=subprocess_env, check=True)
 
     # Configuration for the destination PostgreSQL database
@@ -53,16 +53,15 @@ else:
         'host': 'destination_postgres'
     }
         
-    # Use psql to load the dumped SQL file into the destination database
     load_command = [
-        'pgsql',
+        'psql',
         '-h', destination_config['host'],
         '-U', destination_config['user'],
         '-d', destination_config['dbname'],
         '-a', '-f', 'data_dump.sql'
     ]
-
-    subprocess_env = destination_config['password']
+    
+    subprocess_env = dict(PGPASSWORD=destination_config['password'])
     subprocess.run(load_command, env=subprocess_env, check=True)
 
     print("Ending ELT Process...")
